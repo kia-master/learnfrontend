816641511eb258c5a04f3c7499469e11a6448f56
kia-master <kia.iri.kia@gmail.com>
2021-09-25 19:42:24 +0300

    Create page for week-15. Add answers to questions

---
+++ week15-js/scripts/script.js
@@ -0,0 +1,59 @@
+// change color for divs
+
+function changeColorRed() {
+    let div = document.getElementById('color-red');
+    let text = document.getElementById('text-red');
+    div.style.backgroundColor = 'orange';
+    text.textContent = 'Orange';
+}
+
+function changeColorGreen() {
+    let div = document.getElementById('color-green');
+    let text = document.getElementById('text-green');
+    div.style.backgroundColor = 'gray';
+    text.textContent = 'Gray';
+}
+
+function changeColorBlue() {
+    let div = document.getElementById('color-blue');
+    let text = document.getElementById('text-blue');
+    div.style.backgroundColor = 'purple';
+    text.textContent = 'Purple';
+}
+
+// gallery
+
+let img = document.querySelector('.gallery-image > img');
+
+function getNextImg() {
+    img.setAttribute("src", "assets/images/img2.jpg")
+}
+
+function getPreviousImg() {
+    img.setAttribute("src", "assets/images/img3.jpg")
+}
+
+// calculator
+
+function getNumberOne() {
+    let num1 = Number(document.getElementById('num1').value);
+    return num1;
+}
+
+function getNumberTwo() {
+    let num2 = Number(document.getElementById('num2').value);
+    return num2;
+}
+
+let result = document.querySelector('#result > p');
+
+let sum = () => result.textContent = getNumberOne() + getNumberTwo();
+let subtract = () => result.innerHTML = getNumberOne() - getNumberTwo();
+let multiply = () => result.innerHTML = getNumberOne() * getNumberTwo();
+let divide = () => result.innerHTML = getNumberOne() / getNumberTwo();
+
+function resetInputCalc() {
+    document.getElementById('num1').value = '';
+    document.getElementById('num2').value = '';
+    document.querySelector('#result > p').textContent = 'Здесь будет результат';
+}
