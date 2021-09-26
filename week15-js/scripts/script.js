ad5d5439f7b4413923e60c4c79a32d6eac4eb6d3
kia-master <kia.iri.kia@gmail.com>
2021-09-26 23:08:36 +0300

    Add checking form for task 3

--- week15-js/scripts/script.js
+++ week15-js/scripts/script.js
@@ -20,17 +20,6 @@ function changeColorBlock() {
             break;
     }
 }
-// gallery
-
-let img = document.querySelector('.gallery-image > img');
-
-function getNextImg() {
-    img.setAttribute("src", "assets/images/img2.jpg")
-}
-
-function getPreviousImg() {
-    img.setAttribute("src", "assets/images/img3.jpg")
-}
 
 // calculator
 
@@ -64,3 +53,81 @@ function resetInputCalc() {
     result.style.color = "";
     result.style.fontWeight = "";
 }
+
+// registration
+const form = document.getElementById('form');
+
+const welc = document.getElementById("welcome");
+const info = document.getElementById("info_user");
+
+const firstName = document.getElementById("firstname");
+const firstNameError = document.getElementById("warnname");
+
+const surname = document.getElementById("surname");
+const surnameError = document.getElementById("warnsurname");
+
+const password = document.getElementById("password");
+const passwordError = document.getElementById("warnpass");
+
+const passRepeat = document.getElementById("passrepeat");
+const passrepeatError = document.getElementById("warnpassrep");
+
+const email = document.getElementById("email");
+const emailError = document.getElementById("warnmail");
+
+var checkGender = document.querySelectorAll('input[type="radio"]:checked');
+var valueGender = checkGender.length > 0 ? checkGender[0].value : null;
+const checkedGenderError = document.getElementById("warngender");
+
+const country = document.getElementById("country");
+const countryError = document.getElementById("warncountry");
+
+function checkEmpty(v, err) {
+    if (v.value == "") {
+        err.innerText = "Поле не должно пустым!";
+    } else {
+        err.innerText = "";
+    }
+}
+
+function comparePass(first, second, err) {
+    if (first.value !== second.value) {
+        err.innerText = "Пароли не совпадают!";
+    } else if (second.value == "") {
+        err.innerText = "Поле не должно пустым!";
+    } else {
+        err.innerText = "";
+    }
+}
+
+function checkSelect(sel, err) {
+    let selectedCountry = sel.options[sel.selectedIndex].value;
+    if (selectedCountry == 0) {
+        err.innerText = "Выберите страну";
+    } else {
+        err.innerText = "";
+    }
+}
+
+function checkRadio(box, err) {
+    if (box != null) {
+        err.innerText = "";
+    } else {
+        err.innerText = "Выберите пол";
+    }
+}
+
+form.addEventListener('submit', (event) => {
+
+    checkEmpty(firstName, firstNameError);
+    checkEmpty(surname, surnameError);
+    checkEmpty(password, passwordError);
+    checkEmpty(passRepeat, passrepeatError);
+    checkEmpty(email, emailError);
+
+    comparePass(password, passRepeat, passrepeatError);
+    checkSelect(country, countryError);
+    checkRadio(valueGender, checkedGenderError);
+
+    event.preventDefault();
+});
